57239edf2058570c3e93ec9b73ce98570b276e2f
danngo-sama <ly1026hj@outlook.com>
2024-03-26 22:29:32 +0800

    RegressionPrediction.py

---
+++ bill_recognize_system/data_calculate/__init__.py
@@ -0,0 +1,10 @@
+"""
+The :mod:`bill_recognize_system.data_calculate` module includes some
+methods for predicting data
+"""
+
+from RegressionPrediction import RegressionPrediction
+
+__all__: [
+    "RegressionPrediction",
+]
